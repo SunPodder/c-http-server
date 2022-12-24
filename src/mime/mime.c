1fce4146458644a813b6504263ad22ef8cfcd0de
Sunlight <73415680+SunPodder@users.noreply.github.com>
2022-12-24 17:55:06 +0600

    better mime table with huge collection

--- src/mime/mime.c
+++ src/mime/mime.c
@@ -1,8 +1,7 @@
 #include <string.h>
 #include "mime.h"
-#include "../hash-table/hash-table.h"
+#include "mime-table/mime-lookup.def"
 
-ht_t* table;
 
 static char *getFileExtension(char *filename) {
     char *dot = strrchr(filename, '.');
@@ -10,23 +9,11 @@ static char *getFileExtension(char *filename) {
     return dot + 1;
 }
 
-static void init_mime_table(){
-    table = ht_create(10);
-
-    ht_set(table, "html", "text/html");
-    ht_set(table, "css", "text/css");
-    ht_set(table, "js", "text/javascript");
-    ht_set(table, "png", "image/png");
-    ht_set(table, "jpg", "image/jpg");
-    ht_set(table, "jpeg", "image/jpeg");
-    ht_set(table, "ico", "image/vnd.microsoft");
-    ht_set(table, "svg", "image/svg+xml");
-    ht_set(table, "json", "application/json");
-}
 
 char* getMimeType(char *filename){
     char *extension = getFileExtension(filename);
-    char* type = ht_get(table, extension);
+    
+    char* type = in_word_set(extension, strlen(extension)) -> mime; 
 
     if(type == NULL){
         return "text/plain";
@@ -34,5 +21,4 @@ char* getMimeType(char *filename){
     return type;
 }
 
-#define init_mime_table call function
 #define getFileExtension call function
